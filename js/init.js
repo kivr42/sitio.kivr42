89b640cf24ccd099638180ee6cecacf7811bab5c
kivr42 <82487856+kivr42@users.noreply.github.com>
2021-04-14 07:25:31 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+   apiKey: "AIzaSyCHWyRj97ftIxg-0jz5SYMzqRibUxzwj10",
+    authDomain: "bdfalum.firebaseapp.com",
+    projectId: "bdfalum",
+    storageBucket: "bdfalum.appspot.com",
+    messagingSenderId: "279962195015",
+    appId: "1:279962195015:web:3e70fdf3c440afb8a7cd62",
+    measurementId: "G-SG40SGRFR6"
 });
