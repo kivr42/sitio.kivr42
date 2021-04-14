52462cc55f80e04792fdc6653fe4d29c4138ddf5
kivr42 <82487856+kivr42@users.noreply.github.com>
2021-04-14 06:42:38 -0500

    Create CtrlAlumnos.js

---
+++ js/CtrlAlumnos.js
@@ -0,0 +1,32 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  cod,
+  muestraError
+} from "../lib/util.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+/** @type {HTMLUListElement} */
+const lista = document.
+  querySelector("#lista");
+const daoAlumno =
+  getFirestore().
+    collection("Alumno");
+
+getAuth().
+  onAuthStateChanged(
+    protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    consulta();
+  }
+}
